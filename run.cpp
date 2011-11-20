2b57455a07656ef902b7d4b34fb747ed4bbd7da5
Andrei Taranchenko <andrei360-git@yahoo.com>
2011-11-20 17:32:46 -0500

    Luhn test, but does not mask it yet.

--- run.cpp
+++ run.cpp
@@ -5,7 +5,10 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <numeric>
 #include <ctype.h>
+#include <sstream> 
+
 #endif
 
 using namespace std;
@@ -14,7 +17,7 @@ const int MIN_LEN = 14;
 const int MAX_LEN = 16;
 
 string mask(const string data);
-bool is_valid(vector<char> data);
+bool is_valid(vector<int> data);
 
 int main(int argc, char* argv[]) {
     string data;
@@ -28,12 +31,12 @@ int main(int argc, char* argv[]) {
 }
 //----------------------------------------------------------------------
 
-void print (char ch) {
+void print (int ch) {
     cerr << ch;
 }
 //----------------------------------------------------------------------
 
-void dump(vector<char> data) {
+void dump(vector<int> data) {
     for_each(data.begin(), data.end(), print);
 }
 //----------------------------------------------------------------------
@@ -41,49 +44,80 @@ void dump(vector<char> data) {
 string mask(const string data) {
     cerr << "IN: " << data << "\n----------------------------------------------\n";
 
-    vector<char> digits;
+    vector<int> digits;
 
     for(int n=0; n < data.length(); ++n) {
         char ch = data[n]; 
         
-        cerr << "Processing: " << ch;
+        //cerr << "Processing: " << ch;
         if (!isdigit(ch) && ch != '-' && ch != ' ') {
-            cerr <<  ". Skipping" << endl;
+            //cerr <<  ". Skipping" << endl;
             digits.clear();
             continue;
         }
 
         if (ch == '-' || ch == ' ') {
-            cerr << ". Skip but continue on " << ch << endl;
+            //cerr << ". Skip but continue on " << ch << endl;
             continue;
         }
 
-        cerr << ". Adding: " << ch << endl;
-        digits.push_back(ch);
+        //cerr << ". Adding: " << ch << endl;
+        int digit = atoi(&ch);
+        digits.push_back(digit);
         
-        dump(digits);
-        cerr <<  " - " << digits.size() <<  endl;
+        //dump(digits);
+        //cerr <<  " - " << digits.size() <<  endl;
 
-        const bool valid = is_valid(digits);
+        if (digits.size() >= MIN_LEN && digits.size() <= MAX_LEN) {
+            const bool valid = is_valid(digits);
 
-        dump(digits);
-        if (valid) {
-            cerr << " accepted";
-        } 
-        else {
-            cerr << " rejected";
+            cerr << endl;
+            if (valid) {
+                cerr << "ACCEPTED";
+            } 
+            else {
+                cerr << "REJECTED";
+            }
+            cerr << endl;
         }
-        cerr << endl;
     }
 
     return data;
 }
 //----------------------------------------------------------------------
 
-bool is_valid(const vector<char> data) {
-    if (data.size() < MIN_LEN || data.size() > MAX_LEN) {
-        return false;
-    }
+bool is_valid(vector<int> data) {
+    cerr << "processing: ";
+    dump(data);
+    cerr << endl;
 
-    return true;
+    vector<int> luhn_product;
+
+    vector<int>::reverse_iterator itr = data.rbegin();
+
+    std::stringstream ss;
+    for (itr; itr < data.rend(); ++itr) {
+        int digit = *itr;
+
+        const int dist = distance(data.rbegin(), itr);
+
+        if (dist % 2 != 0) {
+            digit = digit * 2;
+        }
+
+        ss.str("");
+        ss << digit;
+        const string as_string = ss.str();
+
+        for (int n = 0; n < as_string.size(); ++n) {
+            char ch = as_string[n];
+            int num = atoi(&ch);
+            luhn_product.push_back(num);
+        }
+
+    }
+    
+    const int luhn_sum = accumulate(luhn_product.begin(), luhn_product.end(), 0);
+
+    return luhn_sum % 10 == 0;
 }
